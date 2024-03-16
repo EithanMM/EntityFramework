2544bb18d60c0297aa574ce585d1aea827c825e4
Eithan Méndez Méndez <eithan.mndez@gmail.com>
2024-03-16 12:37:55 -0600

    Implementation of UnitOfWork

---
+++ Store.Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Data.Repositories;
+
+public sealed class UnitOfWork : IUnitOfWork
+{
+    private DbContext _dbContext { get; init; }
+
+    public UnitOfWork(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public Task<int> CommitAsync()
+    {
+        return _dbContext.SaveChangesAsync();
+    }
+}
