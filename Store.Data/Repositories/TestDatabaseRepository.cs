bc8cb5b0c0871686787047bc27e1350cdccd1f25
Eithan Méndez Méndez <eithan.mndez@gmail.com>
2024-03-16 21:05:18 -0600

    Implemented database repository

---
+++ Store.Data/Repositories/TestDatabaseRepository.cs
@@ -0,0 +1,8 @@
+﻿using Store.Data.Entities.Store;
+
+namespace Store.Data.Repositories;
+
+public class TestDatabaseRepository(TestDatabaseDbContext dbContext) : Repository<TestDatabaseDbContext>(dbContext), ITestDatabaseRepository
+{
+    public IQueryable<StoreEntity> Stores => DbContext.Stores;
+}
